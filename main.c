15669556ca9f3ea04762443c591b94696b72689f
Ivan Leopoldo <ivanleopoldoc@gmail.com>
2023-02-12 23:10:24 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100
+
+typedef struct node {
+    float data;
+    struct node *next;
+} Stack;
+
+Stack *top = NULL;
+char postfix[MAX] = "";
+
+void push(float);
+float pop();
+void display();
+float evaluate(char*);
+
+void main(){
+    char equation[MAX];
+    float result;
+    printf("Input Equation: ");
+    scanf("%s", equation);
+
+    result = evaluate(equation);
+
+    printf("\n");
+    printf("result:\n%s = %f", equation, result);
+}
+
+float evaluate(char *equation){
+    int i=0;
+    char temp;
+
+    for(;i<strlen(equation);i++){
+        if(isdigit(equation[i])){
+            strncat(postfix, &equation[i], 1);
+        } else if(equation[i] == ')'){
+            if(top!=NULL){
+                temp = (int)pop();
+                strncat(postfix, &temp, 1);
+            }
+        } else if(equation[i] != '(') {
+            push(equation[i]);
+        }
+    }
+
+    while(top!=NULL){
+        temp = pop();
+        strncat(postfix, &temp, 1);
+    }
+
+    int k=0;
+    float A, B;
+    for(; postfix[k]!='\0'; k++){
+        if(isdigit(postfix[k])){
+            int temp = postfix[k] - '0';
+            push(temp);
+        } else {
+            A = pop();
+            B = pop();
+            switch(postfix[k]){
+                case '+':{
+                    push(A+B);
+                    break;
+                }
+                case '-':{
+                    push(A-B);
+                    break;
+                }
+                case '*':{
+                    push(A*B);
+                    break;
+                }
+                case '/':{
+                    push(B/A);
+                    break;
+                }
+            }
+        }
+    }
+    return pop();
+}
+
+void push(float data){
+    if(top==NULL){
+        top = (Stack*)malloc(sizeof(Stack*));
+        top->data = data;
+        top->next = NULL;
+    } else {
+        Stack *temp = (Stack*)malloc(sizeof(Stack*));
+        temp->data = data;
+        temp->next = top;
+        top = temp;
+    }
+}
+
+float pop(){
+    if(top==NULL){
+        printf("Empty Stack");
+    } else {
+        Stack *temp=top;
+        float data = top->data;
+        temp=temp->next;
+        free(top);
+        top=temp;
+        return data;
+    }
+}
